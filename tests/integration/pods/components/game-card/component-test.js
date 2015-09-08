ea077b9c711ed8149dac0d0d35d3a4ad547f9709
Jake Mays <quaunaut@gmail.com>
2015-09-08 13:36:33 -0600

    Updated tests to template changes.

--- tests/integration/pods/components/game-card/component-test.js
+++ tests/integration/pods/components/game-card/component-test.js
@@ -1,8 +1,12 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import startMirage from '../../../../helpers/setup-mirage-for-integration';
 
 moduleForComponent('game-card', 'Integration | Component | game card', {
-  integration: true
+  integration: true,
+  setup: function() {
+    startMirage(this.container);
+  }
 });
 
 test('it renders', function(assert) {
@@ -10,11 +14,13 @@ test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  // var game = server.create('game');
+  // this.set('game', game);
 
   this.render(hbs`{{game-card}}`);
 
   // this is nonsense
-  assert.equal(this.$().text().trim(), 'vs.');
+  assert.equal(this.$('.youtubeUrl').text(), 'No Youtube Link');
 
   // Template block usage:
   // this.render(hbs`
--- tests/integration/pods/components/tournament-list/component-test.js
+++ tests/integration/pods/components/tournament-list/component-test.js
@@ -1,19 +1,37 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import startMirage from '../../../../helpers/setup-mirage-for-integration';
 
 moduleForComponent('tournament-list', 'Integration | Component | tournaments list', {
-  integration: true
+  integration: true,
+  setup: function() {
+    startMirage(this.container);
+  }
 });
 
 test('it renders', function(assert) {
   assert.expect(1);
 
+  // So, this has been a fight to hell and back. Punting for now,
+  // as Mirage can't properly handle JSONAPI and using
+  // Ember Data models in integration tests is nothing but
+  // trial and error from absolutely absent documentation. Ugh.
+  // ===============================
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  // var tourneys = server.createList('tournament', 5),
+  //     tournaments = [];
+  // tourneys.forEach(function(tourney) {
+  //   var tournament = store.createRecord('tournament', tourney);
+  //   tournaments.push(tournament);
+  // });
+  //
+  //
+  // this.set('tournaments', tournaments);
 
-  this.render(hbs`{{tournament-list}}`);
+  this.render(hbs`{{tournament-list tournaments=tournaments}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('.tournament').length, 1);
 
   // Template block usage:
   // this.render(hbs`
