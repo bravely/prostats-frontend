784dbb82680b7526d281c8016608610ece24c804
Jake Mays <quaunaut@gmail.com>
2015-09-08 13:41:28 -0600

    Removed extraneous mirage attr, added link to actual game show page.

--- app/mirage/factories/tournament.js
+++ app/mirage/factories/tournament.js
@@ -6,6 +6,5 @@ export default Mirage.Factory.extend({
   'ends-at': faker.date.recent,
   finished: faker.list.random(true, false),
   season: faker.company.companyName,
-  'last-played-at': faker.date.past,
-  lastPlayedAt: faker.date.past
+  'last-played-at': faker.date.past
 });
